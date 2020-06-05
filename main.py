6a6cb241a16e87ee7f2189e704dc3376de76ca1d
MOCHY\unpok <crycli87+github.com@gmail.com>
2020-06-05 20:22:24 +0900

    基礎設計途中

---
+++ main.py
@@ -0,0 +1,13 @@
+class Emulator:
+    None
+class Environment:
+    # レジスタは辞書で持ったほうがいい気がする
+    self.registers =
+if __name__ == '__main__':
+    """
+    # コマンドから実行バイナリのパスを取得
+    while(Emu.env.eip!=0):
+        (prefix,opcode,modrm,sib,displacement,imidiate) = fetch()
+        Emulator.env = exec()
+    print_env(Emulator.env)
+    """
